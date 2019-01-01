ba97e02f6755c7ad1a527016eb51e1318cdd5d1c
Thomas Castleman <thomas7rex@gmail.com>
2019-01-01 09:57:30 -0500

    Connect MIDI input to harmoniser

--- src/Main.java
+++ src/Main.java
@@ -15,73 +15,82 @@ import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		NoteHandler handler = new NoteHandler();
-		
-		MidiBus.list();
-		MidiBus bus = new MidiBus(handler, 0, 1);
-	}
+	// MIDI keyboard parameters
+	public static final int MIN_PITCH = 48, MAX_PITCH = 84;
+	
+	// frequencies coming from MIDI instrument
+	public static float[] targetFrequencies;
 
-//	public static void main(String[] args) throws LineUnavailableException {		
-//		int BUFFER_SIZE = 8192; // 1024;
-//		int SAMPLE_RATE = 44100;
-//		
-//		// set up audio stream from default mic
-//		AudioDispatcher d = AudioDispatcherFactory.fromDefaultMicrophone(BUFFER_SIZE, 0);
-//		
-//		// remove any frequencies below 110 Hz
-//		d.addAudioProcessor(new HighPass(110, SAMPLE_RATE));
-//		
-//		// create new PSOLA object for pitch shifting
-//		PSOLA psola = new PSOLA(BUFFER_SIZE, SAMPLE_RATE);
-//		
-//		float[] targetFrequencies = {233.08f, 293.66f, 349.23f, 440.0f};
-//
-//		// setup new PitchDetection handler
-//		PitchDetectionHandler handler = new PitchDetectionHandler() {
-//	        @Override
-//	        public void handlePitch(PitchDetectionResult result, AudioEvent audioEvent) {
-//	        	// if pitch detected
-//	        	if (result.getPitch() != -1) {	        		
-//	        		// analyze mic audio using pitch estimate
-//	        		psola.analyze(audioEvent.getFloatBuffer(), result.getPitch());
-//	        		
-//	        		float[] avgOut = new float[BUFFER_SIZE], shifted;
-//	        		
-//	        		// calculate each pitch shifted buffer, adding to average
-//	        		for (int i = 0; i < targetFrequencies.length; i++) {
-//	        			shifted = psola.shift(targetFrequencies[i]);
-//	        			
-//	        			// add to average buffer
-//	        			for (int j = 0; j < shifted.length; j++) {
-//	        				avgOut[j] += shifted[j];
-//	        			}
-//	        		}
-//	        		
-//	        		// finalize average by dividing by number of separate frequencies
-//	        		for (int i = 0; i < avgOut.length; i++) {
-//	        			avgOut[i] /= targetFrequencies.length;
-//	        		}
-//	        		
-//	        		// replace audio with synthesized signal
-//	        		audioEvent.setFloatBuffer(avgOut);
-//	        		
-//	        	// if no pitch detected
-//	        	} else {
-//	        		// do not playback any audio
-//	        		audioEvent.clearFloatBuffer();
-//	        	}
-//	        }
-//	    };
-//	    
-//	    // add pitch detection with YIN algorithm, using above handler
-//	    d.addAudioProcessor(new PitchProcessor(PitchEstimationAlgorithm.YIN, SAMPLE_RATE, BUFFER_SIZE, handler));
-//	    
-//	    // add an audio player that plays back audio in realtime
-//	    d.addAudioProcessor(new AudioPlayer(new AudioFormat(SAMPLE_RATE, 16, 1, true, true)));
-//	    
-//	    // run audio dispatcher
-//	    d.run();
-//	}
+	public static void main(String[] args) throws LineUnavailableException {		
+		int BUFFER_SIZE = 8192; // 1024;
+		int SAMPLE_RATE = 44100;
+		
+		// set up MIDI handlers
+		NoteHandler noteHandler = new NoteHandler(MIN_PITCH, MAX_PITCH);
+		MidiBus.list();
+		MidiBus bus = new MidiBus(noteHandler, 0, 1);
+		
+		// set up audio stream from default mic
+		AudioDispatcher d = AudioDispatcherFactory.fromDefaultMicrophone(BUFFER_SIZE, 0);
+		
+		// remove any frequencies below 110 Hz
+		d.addAudioProcessor(new HighPass(110, SAMPLE_RATE));
+		
+		// create new PSOLA object for pitch shifting
+		PSOLA psola = new PSOLA(BUFFER_SIZE, SAMPLE_RATE);
+		
+		// float[] targetFrequencies; //  = {233.08f, 293.66f, 349.23f, 440.0f};
+
+		// setup new PitchDetection handler
+		PitchDetectionHandler handler = new PitchDetectionHandler() {
+	        @Override
+	        public void handlePitch(PitchDetectionResult result, AudioEvent audioEvent) {
+	        	// if pitch detected
+	        	if (result.getPitch() != -1) {	        		
+	        		// analyze mic audio using pitch estimate
+	        		psola.analyze(audioEvent.getFloatBuffer(), result.getPitch());
+	        		
+	        		float[] avgOut = new float[BUFFER_SIZE], shifted;
+	        		
+	        		// get frequencies being played on keyboard
+	        		targetFrequencies = noteHandler.getFrequencies();
+	        		
+	        		// calculate each pitch shifted buffer, adding to average
+	        		for (int i = 0; i < targetFrequencies.length; i++) {
+	        			if (targetFrequencies[i] > 0) {
+		        			shifted = psola.shift(targetFrequencies[i]);
+		        			
+		        			// add to average buffer
+		        			for (int j = 0; j < shifted.length; j++) {
+		        				avgOut[j] += shifted[j];
+		        			}
+	        			}
+	        		}
+	        		
+	        		// finalize average by dividing by number of separate frequencies
+	        		for (int i = 0; i < avgOut.length; i++) {
+	        			avgOut[i] /= targetFrequencies.length;
+	        		}
+	        		
+	        		// replace audio with synthesized signal
+	        		audioEvent.setFloatBuffer(avgOut);
+	        		
+	        	// if no pitch detected
+	        	} else {
+	        		// do not playback any audio
+	        		audioEvent.clearFloatBuffer();
+	        	}
+	        }
+	    };
+	    
+	    // add pitch detection with YIN algorithm, using above handler
+	    d.addAudioProcessor(new PitchProcessor(PitchEstimationAlgorithm.YIN, SAMPLE_RATE, BUFFER_SIZE, handler));
+	    
+	    // add an audio player that plays back audio in realtime
+	    d.addAudioProcessor(new AudioPlayer(new AudioFormat(SAMPLE_RATE, 16, 1, true, true)));
+	    
+	    // run audio dispatcher
+	    d.run();
+	}
 
 }
--- src/NoteHandler.java
+++ src/NoteHandler.java
@@ -1,50 +1,71 @@
-/**
- * Copyright (c) 2009 Severin Smith
- *
- * This file is part of a library called The MidiBus (themidibus) - http://www.smallbutdigital.com/themidibus.php.
- *
- * The MidiBus is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The MidiBus is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the MidiBus. If not, see <http://www.gnu.org/licenses/>.
-*/
 
-public class NoteHandler {	
+public class NoteHandler {
+	
+	private int[] keys;
+	private int MIN_PITCH, MAX_PITCH;
+	private int numActive;
 	
 	// construct new NoteHandler
-	public NoteHandler() {
+	public NoteHandler(int MIN_PITCH, int MAX_PITCH) {
+		this.MIN_PITCH = MIN_PITCH;
+		this.MAX_PITCH = MAX_PITCH;
+		this.keys = new int[MAX_PITCH - MIN_PITCH + 1];
+		this.numActive = 0;
+	}
+	
+	// convert from MIDI pitch number to frequency in Hz (taken from TarsosDSP, https://github.com/JorenSix/TarsosDSP/blob/master/src/core/be/tarsos/dsp/util/PitchConverter.java
+	public static double midiCentToHertz(final double midiCent) {
+		return 440 * Math.pow(2, (midiCent - 69) / 12d);
+	}
+	
+	// get the corresponding frequencies of notes currently being held
+	public float[] getFrequencies() {
+		float[] frequencies = new float[this.numActive];
+		int midiNum;
 		
+		// if any notes are active
+		if (frequencies.length > 0) {
+			int j = 0;
+			// for each note
+			for (int i = 0; i < this.keys.length; i++) {
+				// if active note
+				if (this.keys[i] == 1) {
+					midiNum = i + this.MIN_PITCH;
+					
+					// if valid MIDI number
+					if (midiNum > -1 && midiNum < 128) {
+						frequencies[j++] = (float) midiCentToHertz(midiNum);
+					} else {
+						frequencies[j++] = 0.0f;
+					}
+				}
+			}
+		}
+		
+		return frequencies;
 	}
 	
 	/**
-	 * Is passed the channel, pitch and velocity associated with every new NoteOn MIDI message recieved by a MidiBus attached to this applet.
+	 * Is passed the channel, pitch and velocity associated with every new NoteOn MIDI message received by a MidiBus
 	 *
 	 * @param channel the channel on which the NoteOn arrived
 	 * @param pitch the pitch associated with the NoteOn
 	 * @param velocity the velocity associated with the NoteOn
-	 * @see #noteOn(int channel, int pitch, int velocity, long timestamp, String bus_name)
 	*/
 	public void noteOn(int channel, int pitch, int velocity) {
-		System.out.println("ON: " + pitch + ", " + velocity);
+		this.keys[pitch - MIN_PITCH] = 1;	// mark active note with 1
+		this.numActive++;					// increase number of active notes
 	}
 	
 	/**
-	 * Is passed the channel, pitch and velocity associated with every new NoteOff MIDI message recieved by a MidiBus attached to this applet.
+	 * Is passed the channel, pitch and velocity associated with every new NoteOff MIDI message received by a MidiBus
 	 *
 	 * @param channel the channel on which the NoteOff arrived
 	 * @param pitch the pitch associated with the NoteOff
 	 * @param velocity the velocity associated with the NoteOff
-	 * @see #noteOff(int channel, int pitch, int velocity, long timestamp, String bus_name)
 	*/
 	public void noteOff(int channel, int pitch, int velocity) {
-		System.out.println("OFF: " + pitch + ", " + velocity);
+		this.keys[pitch - MIN_PITCH] = 0;			// mark inactive note with a 0
+		if (this.numActive > 0) this.numActive--;	// decrease number of active notes
 	}
 }
